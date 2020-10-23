f7d462565337f782a2f6f32d8b1a9e9921685736
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2020-10-22 20:43:58 -0400

    Create 102 LevelOrderTraversal.py

---
+++ BFS/102 LevelOrderTraversal.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def levelOrder(self, root):
+        result = []
+        q = deque()
+        if root:
+            q.append(root)
+        while q:
+            curLevel = []
+            for i in range(len(q)):
+                node = q.popleft()
+                curLevel.append(node.val)
+                if node.left: 
+                    q.append(node.left)
+                if node.right: 
+                    q.append(node.right)
+            result.append(curLevel)
+        return result
