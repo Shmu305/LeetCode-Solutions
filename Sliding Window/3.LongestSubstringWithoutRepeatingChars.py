0e1efa2189671738b61ece8419535d0c13eb4716
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-01-03 20:17:29 -0500

    Update 3.LongestSubstringWithoutRepeatingChars.py

--- Sliding Window/3.LongestSubstringWithoutRepeatingChars.py
+++ Sliding Window/3.LongestSubstringWithoutRepeatingChars.py
@@ -8,3 +8,18 @@
                 maxLength = max(maxLength, index - start + 1)
 	    usedChar[char] = index
         return maxLength
+
+
+##less efficient approach
+	longest = 0
+	left, right = 0, 0
+        chars = set()
+        while left < len(string) and right < len(string):
+            if string[right] not in chars:
+                chars.add(string[right])
+                right += 1
+                longest = max(longest, right - left)
+            else:
+                chars.remove(string[left])
+                left += 1
+        return longest
