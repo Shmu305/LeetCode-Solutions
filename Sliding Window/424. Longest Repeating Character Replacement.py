b4d8a0bbe20932ee52285e8e6d125665c1fb96bf
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-01-05 23:23:06 -0500

    Update 424. Longest Repeating Character Replacement.py

--- Sliding Window/424. Longest Repeating Character Replacement.py
+++ Sliding Window/424. Longest Repeating Character Replacement.py
@@ -24,3 +24,17 @@ class Solution:
             res = max(res, end - start + 1)
         
         return res
+    
+   ## using dict instead
+
+      def characterReplacement(self, s, k):
+        count = {}
+        max_count = start = result = 0
+        for end in range(len(s)):
+            count[s[end]] = count.get(s[end], 0) + 1
+            max_count = max(max_count, count[s[end]])
+            if end - start + 1 - max_count > k:
+                count[s[start]] -= 1
+                start += 1
+            result = max(result, end - start + 1)
+        return result
