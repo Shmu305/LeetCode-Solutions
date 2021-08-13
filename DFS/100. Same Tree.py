bdfe9d1084b9cac5182b3a00ab881732c820fe6a
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-08-13 18:20:47 -0400

    Update 100. Same Tree.py

--- DFS/100. Same Tree.py
+++ DFS/100. Same Tree.py
@@ -4,3 +4,26 @@
             return False
         else:
             return (p.val ==q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(q.right, p.right) 
+
+#iterative BFS
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        deq = collections.deque()
+        deq.append((p, q))
+        while deq:
+            p, q = deq.popleft()
+            if not p and not q:
+                continue
+            if not p or not q or p.val != q.val:
+                return False
+
+            deq.append((p.left, q.left))
+       
+            deq.append((p.right, q.right))
+        return True
