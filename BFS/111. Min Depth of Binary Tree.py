43f5364e4805bd6251eaf39a83c4531d34b27742
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-09-19 16:37:10 -0400

    Update 111. Min Depth of Binary Tree.py

--- BFS/111. Min Depth of Binary Tree.py
+++ BFS/111. Min Depth of Binary Tree.py
@@ -12,5 +12,25 @@
             if node.right:
                 q.append((node.right, depth+1))
                 
-        
-        
+#another way
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        q = deque()
+        if root:
+            q.append(root)
+        minDepth = 0
+        while q:
+            minDepth += 1
+            for i in range(len(q)):
+                node = q.popleft()
+                if not node.left and not node.right:
+                    return minDepth
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return minDepth
+            
