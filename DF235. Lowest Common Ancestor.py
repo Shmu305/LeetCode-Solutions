5b22761187589559fbc0319c2215dec42234b74b
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-09-18 23:05:32 -0400

    Create DF235. Lowest Common Ancestor.py

---
+++ DF235. Lowest Common Ancestor.py
@@ -0,0 +1,17 @@
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+       
+        
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+            
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        else: 
+            return root
