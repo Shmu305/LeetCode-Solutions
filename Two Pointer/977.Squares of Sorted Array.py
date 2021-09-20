59d2475d9e03ad33c7d4198f072f349843453f3a
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-09-19 22:50:46 -0400

    Create 977.Squares of Sorted Array.py

---
+++ Two Pointer/977.Squares of Sorted Array.py
@@ -0,0 +1,16 @@
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [0] * len(nums)
+        l , r = 0 , len(nums)-1
+        while l <= r:
+            left, right = abs(nums[l]), abs(nums[r])
+            if left > right:
+                ans[r-l] = left*left 
+                l += 1
+            else:
+                ans[r-l] = right*right
+                r -= 1
+        return ans
