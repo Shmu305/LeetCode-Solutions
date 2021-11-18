e5d06d12a0b9eb4942e80a40d5586984f088461e
Shmu305 <44219888+Shmu305@users.noreply.github.com>
2021-11-18 18:35:44 -0500

    Create 424. Longest Repeating Character Replacement.py

---
+++ Blind75/String/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,14 @@
+#Time: O(n)
+#Space: O(n)
+class Solution(object):
+    def characterReplacement(self, s, k):
+        count = {}
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r-l+1)
+        return res
